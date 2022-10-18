61d395be0abcc87f57dc98b34a328f3c529b65e5
Aleksey Macegora <alekseymacegora@MacBook-Air-Aleksey.local>
2022-10-19 00:43:10 +0300

    initial commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// Application theme data
+class ClockAppTheme {
+  /// Get default theme
+  static ThemeData get defaultTheme => ThemeData();
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:clock_app/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ClockAppTheme.defaultTheme,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
